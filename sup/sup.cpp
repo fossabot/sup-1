d59d597c8973406fa6a9c1fa20452737d5db773f
Aleks Rutins <keeper@farthergate.com>
2025-12-22 15:31:48 -0500

    change the way memory safety works a bit

--- sup/sup.cpp
+++ sup/sup.cpp
@@ -2,23 +2,36 @@
 #include <set>
 using namespace std;
 
-static set<Sup *> __unsafe_ptrs;
+static set<Sup *> __safe_ptrs;
+
+class _PtrCleanup {
+    ~_PtrCleanup() {
+        for (auto p : __safe_ptrs) {
+            __safe_ptrs.erase(p);
+            if(p) delete p;
+        }
+    }
+    static _PtrCleanup _;
+};
+
+_PtrCleanup _PtrCleanup::_;
 
 Sup::Sup(Sup *parent)
     : _parent(parent)
 {
-    if(__unsafe_ptrs.contains(this)) __unsafe_ptrs.erase(this);
+    __safe_ptrs.insert(this);
     if(parent) {
         parent->addChild(this);
     }
 }
 
 Sup::~Sup() {
+    __safe_ptrs.erase(this);
     if(_parent) _parent->remove(this);
     for (auto c : _children) {
-        if(c && c != this && !(__unsafe_ptrs.contains(c))) {
+        if(c && c != this && __safe_ptrs.contains(c)) {
             delete c;
-            __unsafe_ptrs.insert(c);
+            __safe_ptrs.erase(c);
         }
     }
 
